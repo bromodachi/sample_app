f0cbeb1fab5b6037aa518056875945d866991c3c
bromodachi <curaga1@gmail.com>
2015-06-10 10:15:11 +0000

    in chapter 7, finished signup page. need to resume from 7.3.4

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Ruby on Rails Tutorial: sample application
 
-This is the sample application leared through the 
+This is the sample application learned through the 
 [*Ruby on Rails Tutorial:
 Learn Web Development with Rails*](http://www.railstutorial.org/).
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+  def create
+   @user = User.new(user_params)
+    if @user.save #if I can save the user, 
+    else 
+      render 'new' #else, go back to the new method
+    end
+  end
+  
+  private 
+    def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
